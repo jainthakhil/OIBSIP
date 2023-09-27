63c400669103d274ab0672ef4dbae37a0476eb40
Akhil Jainth <122777560+jainthakhil@users.noreply.github.com>
2023-09-27 23:05:21 +0530

    uploaded

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        navLinks.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
